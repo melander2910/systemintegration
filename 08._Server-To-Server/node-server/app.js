8c76a1f4e31dd673298218eb3cdfe9922bf37b50
Peter Dalshaard Melander <petermelander2910@gmail.com>
2024-03-20 16:55:06 +0100

    timetracking webhook

--- 08._Server-To-Server/node-server/app.js
+++ 08._Server-To-Server/node-server/app.js
@@ -3,6 +3,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/requestfastapi', async (request, response) => {
     const res = await fetch("http://127.0.0.1:8000/");
     var data = await res.json();
@@ -12,10 +14,18 @@ app.get('/requestfastapi', async (request, response) => {
 });
 
 app.get('/', async (request, response) => {
+    
     response.send({"data": "node"});
 });
 
-const PORT = 8080;
+app.post('/weekend', async (req, res) => {
+
+    var gg = req.body
+    console.log(gg);
+    res.send(gg);
+})
+
+const PORT = 5555;
 app.listen(PORT, () => {
     console.log('Server is running on port', PORT);
 })
